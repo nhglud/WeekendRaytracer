a8ee715b2fb815086ad9c241cf31a48f823f5806
nhglud <nhglud@gmail.com>
2025-12-19 13:41:06 +0100

    hit sphere

--- main.cpp
+++ main.cpp
@@ -5,8 +5,26 @@
 
 #include <iostream>
 
+
+bool hit_sphere(const point3& center, double radius, const ray& r)
+{
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return discriminant >= 0;
+}
+
+
 color ray_color(const ray& r)
 {
+
+    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+    {
+        return color(1, 0, 0);
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
@@ -34,19 +52,17 @@ int main()
     auto viewport_u = vec3(viewport_width, 0, 0);
     auto viewport_v = vec3(0, -viewport_height, 0);
 
-    auto pixel_delta_u = viewport_u / viewport_width;
-    auto pixel_delta_v = viewport_v / viewport_height;
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
 
     auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
     auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++)
     {
-        std::clog << "\Scanlines remaining: " << image_height - j << '\n' << std::flush;
+        std::clog << "\rScanlines remaining: " << image_height - j << '\n' << std::flush;
         for (int i = 0; i < image_width; i++)
         {
             auto pixel_center = pixel00_loc + i * pixel_delta_u + j * pixel_delta_v;
