c3762e3f038275975bc66856f283dd3413723be3
nhglud <nhglud@gmail.com>
2025-12-30 12:21:24 +0100

    final image

--- main.cpp
+++ main.cpp
@@ -77,30 +77,4 @@ int main()
     cam.render(world);
     return 0;
 
-	//auto material_ground = make_shared<Lambertian>(Color(0.8, 0.8, 0.0));
-	//auto material_center = make_shared<Lambertian>(Color(0.1, 0.2, 0.5));
-	//auto material_left = make_shared<Dielectric>(0.85);
-	//auto material_right = make_shared<Metal>(Color(0.8, 0.8, 0.6), 0.1);
-
-	//world.add(make_shared<Sphere>(Point3(0.0, -100.5, -1.0), 100.0, material_ground));
-	//world.add(make_shared<Sphere>(Point3(0.0, 0.0, -1.2), 0.5, material_center));
-	//world.add(make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), 0.5, material_left));
-	//world.add(make_shared<Sphere>(Point3(1.0, 0.0, -1.0), 0.5, material_right));
-
-	//Camera cam;
-	//cam.aspect_ratio = 16.0 / 9.0;
-	//cam.image_width = 400;
-	//cam.samples_per_pixel = 100;
-	//cam.max_depth = 50;
-	//cam.lookfrom = Point3(-2, 2, 1);
-	//cam.lookat = Point3(0, 0, -1);
-	//cam.vup = Vec3(0, 1, 0);
-	//cam.vfov = 20;
-
-	//cam.defocus_angle = 10.0;
-	//cam.focus_dist = 3.4;
-
-	//cam.render(world);
-
-	//return 0;
 }
